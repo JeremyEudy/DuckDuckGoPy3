00646540cdf8f7743238ec3a2cad04910d34ec24
Jeremy Eudy <jeremyeudy@gmail.com>
2019-02-14 13:33:10 -0500

    Made progress on Request object in order to catch all possible responses.

--- duckduckpy3.py
+++ duckduckpy3.py
@@ -1,10 +1,43 @@
 #Author: Jeremy Eudy
 
 import json
-
+import requests
+from urllib.parse import urlencode
 
 def search(query, safeSearch=True, html=False, meanings=True):
-    #Define url for API searches
-    searchUrl = "https://api.duckduckgo.com/?q={}&format=json&atb=v1-1".format(query)
+
+    #Catch optional var changes
     safeSearch = '1' if safeSearch else '0'
     html = '0' if html else '1'
+
+    #Define url params for API searches
+    urlParams = {
+            'q' ; query, 
+            'o': 'json', 
+            'kp': safeSearch, 
+            'no_redirect': '1', 
+            'no_html' : html, 
+            'd' : meanings
+            }
+    #Encode params into url format
+    encoded = urlencode(urlParams)
+
+    url = "https://api.duckduckgo.com/?"+encoded
+
+    #get search response
+    r = requests.get(url)
+
+    #jsonify response
+    j = json.loads(r.text)
+
+    return Result(j)
+
+class Result(object):
+
+    def __init__(self, json):
+        self.type = {'A' : 'answer', 'D' : 'disambiguation', 'C' : 'catagory',
+                     'N' : 'name', 'E' : 'exclusive', '' : 'nothing'}.get(json.get('Type',''), '')
+
+        self.json = json
+        self.heading = json.get('Heading', '')
+        self.results = [Result(elem) for elem
